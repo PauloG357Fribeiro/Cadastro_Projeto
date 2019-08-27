0154c8c5ec4a862710c97c04c1480c6cce2bcbe8
PauloG357Fribeiro <paulog25.comercial@gmail.com>
2019-08-27 14:22:58 -0300

    bug

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+        <Main icon="graduation-cap text-danger" title="Sesi Senai"
+            subtitle="Ciência, robótica, inovação e tecnologia para o dia a dia dos nossos alunos">
+            <div className = 'display-4'>Steam</div>
+            <hr />
+            <p className="mb-0">Cadastro</p>
+        </Main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -6,17 +6,13 @@ import 'font-awesome/css/font-awesome.min.css'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 //Main, nele já importa o Header
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
 <div className="app">
     <Logo />
     <Nav />
-    <Main icon="steam text-danger" title="Among Us"
-        subtitle="Jogo disponível na Steam" />
-        <div className = 'display-4'>Steam Ltda.</div>
-            <hr />
-            <p className="mb-0">Criar conta no jogo</p>
+    <Home />
     <Footer />
 </div>
