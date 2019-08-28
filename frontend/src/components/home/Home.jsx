1f0367eee10adf8670727b33092b14971ab7f894
PauloG357Fribeiro <paulog25.comercial@gmail.com>
2019-08-28 14:17:24 -0300

    .

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -4,7 +4,7 @@ import Main from '../template/Main'
 export default props =>
         <Main icon="graduation-cap text-danger" title="Sesi Senai"
             subtitle="Ciência, robótica, inovação e tecnologia para o dia a dia dos nossos alunos">
-            <div className = 'display-4'>Steam</div>
+            <div className = 'display-4'>Usuários</div>
             <hr />
             <p className="mb-0">Cadastro</p>
         </Main>
